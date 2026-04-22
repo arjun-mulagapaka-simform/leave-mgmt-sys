61bb5743ddd3dc32b9eb1909fca63d82690a4767
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-04-22 19:47:19 +0530

    Created app user_mgmt and defined required models with basic testing completed

--- leave_mgmt_sys/settings.py
+++ leave_mgmt_sys/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'user_mgmt'
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user_mgmt.Employee'
---
+++ user_mgmt/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class EmployeeManager(BaseUserManager):
+   '''
+        Custom manager for Employee
+   '''
+   def create_user(self, email, password=None, **extra_fields):
+       if not email:
+           raise ValueError("The Email field must be set")
+       email = self.normalize_email(email)
+       user = self.model(email=email, **extra_fields)
+       user.set_password(password)
+       user.save(using=self._db)
+       return user
+   
+   def create_superuser(self, email, password=None, **extra_fields):
+       extra_fields.setdefault('is_superuser', True)
+       if not extra_fields.get('is_superuser'):
+           raise ValueError("Superuser must have is_superuser=True.")
+       return self.create_user(email, password, **extra_fields)
+
+class Employee(AbstractBaseUser, PermissionsMixin):
+    '''
+        Class for storing employee details
+    '''
+    emp_roles = (
+        ('emp','Employee'),
+        ('rm','Reporting manager'),
+        ('tm','Team manager'),
+        ('hr','HR')
+    )
+    
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    dept = models.ForeignKey('Department',on_delete=models.PROTECT)
+    reporting_mngr = models.ForeignKey('self',on_delete=models.SET_NULL,blank=True,null=True)
+    role = models.CharField(max_length=20,choices=emp_roles)
+    
+    objects = EmployeeManager()
+    
+    USERNAME_FIELD = 'email'
+    
+    class Meta:
+        db_table = 'employee'
+        verbose_name_plural = 'Employees'
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Department(models.Model):
+    '''
+        Class for storing department details
+    '''
+    name = models.CharField(max_length=50,unique=True)
+    mngr = models.OneToOneField('Employee',on_delete=models.SET_NULL,blank=True,null=True,related_name='manager')
+    hr = models.OneToOneField('Employee',on_delete=models.SET_NULL,blank=True,null=True,related_name='hr')
+    
+    class Meta:
+        db_table = 'department'
+        verbose_name_plural = 'departments'
+    
+    def __str__(self):
+        return self.name
