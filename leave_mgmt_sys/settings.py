401e4d65a2d33c92f4cc5438f123317849a9c57e
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-04-24 19:01:50 +0530

    Started leavemanagement app and basic config

--- leave_mgmt_sys/settings.py
+++ leave_mgmt_sys/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'user_mgmt'
+    'user_mgmt',
+    'leavemanagement'
 ]
 
 MIDDLEWARE = [
