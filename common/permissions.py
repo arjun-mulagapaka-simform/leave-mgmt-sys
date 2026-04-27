9c2636e701370d408bd5350904af6dd4f87e3635
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-04-27 19:46:16 +0530

    Fixed logic

--- common/permissions.py
+++ common/permissions.py
@@ -1,13 +1,14 @@
 from rest_framework import permissions
 from user_mgmt.models import *
 
+roles = ['employee','reporting manager','manager','hr']
+
 class IsEmployee(permissions.IsAuthenticated):
     '''
         Authenticate whether requested user is an employee
     '''
     def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name in ['employee','reporting manager','manager','hr']:
+        if request.user.role.name in roles:
             return True
         return False
     
@@ -16,8 +17,7 @@ class IsReportingManager(permissions.IsAuthenticated):
         Authenticate whether requested user is a reporting manager
     '''
     def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'reporting manager':
+        if request.user.role.name == roles[1]:
             return True
         return False
     
@@ -26,8 +26,7 @@ class IsManager(permissions.IsAuthenticated):
         Authenticate whether requested user is a manager
     '''
     def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'manager':
+        if request.user.role.name == roles[2]:
             return True
         return False
     
@@ -36,8 +35,7 @@ class IsHR(permissions.IsAuthenticated):
         Authenticate whether requested user is an HR
     '''
     def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'hr':
+        if request.user.role.name == roles[3]:
             return True
         return False
 
@@ -45,10 +43,9 @@ class IsHrOfDepartment(permissions.IsAuthenticated):
     '''
         Authenticate whether requested user is HR for given department
     '''
-    def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'hr':
-            if user.department.id == request.department.id:
+    def has_object_permission(self, request, view, obj):
+        if request.user.role.name == roles[3]:
+            if request.user.id == obj.id:
                 return True
         return False
     
@@ -56,10 +53,9 @@ class IsManagerOfDepartment(permissions.IsAuthenticated):
     '''
         Authenticate whether requested user is Manager for given department
     '''
-    def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'manager':
-            if user.department.id == request.department.id:
+    def has_object_permission(self, request, view, obj):
+        if request.user.role.name == roles[2]:
+            if request.user.id == obj.id:
                 return True
         return False
 
@@ -67,10 +63,9 @@ class IsReportingManagerOfEmployee(permissions.IsAuthenticated):
     '''
         Authenticate whether requested user is Reporting Manager of given employee
     '''
-    def has_permission(self, request, view):
-        user = Employee.objects.get(pk=request.user.id)
-        if user.role.name == 'reporting manager':
-            if request.employee.reporting_manager.id == user.id:
+    def has_object_permission(self, request, view, obj):
+        if request.user.role.name == roles[1]:
+            if request.user.id == obj.reporting_manager.id:
                 return True
         return False
 
@@ -78,7 +73,7 @@ class IsSelfOrApprover(permissions.IsAuthenticated):
     '''
         Authenticate whether requested user is the owner or approver of requested leave object
     '''
-    def has_permission(self, request, view):
-        if request.user.id == request.leave.employee.id:
+    def has_object_permission(self, request, view, obj):
+        if request.user.id == obj.employee.id:
             return True
         return False
