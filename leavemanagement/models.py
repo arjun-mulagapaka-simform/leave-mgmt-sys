5f9e6a6172aa26ee3b2923cb4de1bc43b5a6f36c
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-04-29 10:04:19 +0530

    Removed duplicate reason

--- leavemanagement/models.py
+++ leavemanagement/models.py
@@ -33,7 +33,6 @@ class LeaveLog(models.Model):
         blank=True,
         null=True,
     )
-    reason = models.CharField(max_length=250)
     actioned_by = models.ForeignKey(
         "user_mgmt.Employee", on_delete=models.DO_NOTHING, blank=True, null=True
     )
